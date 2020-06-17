0dc5ae50b9f65ff592c4e05d26d9ff404d6b7230
menkuichan <ilonaspolding@gmail.com>
2020-06-17 16:58:13 +0300

    feat: UpdateCheckbox component description, add some props to it

--- components/Checkbox/Checkbox.jsx
+++ components/Checkbox/Checkbox.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Checkbox = () => (
-  <input type="checkbox" />
+const Checkbox = ({ label, invalid, disabled }) => (
+  <div>
+    <input
+      type="checkbox"
+      id="checkbox"
+      disabled={disabled}
+      invalid={invalid}
+    />
+    <label htmlFor="checkbox">{label}</label>
+  </div>
 );
+
+Checkbox.defaultProps = {
+  label: '',
+  invalid: false,
+  disabled: false,
+};
+
+Checkbox.propTypes = {
+  label: PropTypes.string,
+  invalid: PropTypes.bool,
+  disabled: PropTypes.bool,
+};
 
 export default Checkbox;
