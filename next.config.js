f29a4988a3a2e454651b3c4a3a580d30db0decee
menkuichan <ilonaspolding@gmail.com>
2020-06-22 16:54:33 +0300

    feat: Update nextjs configuration for deploy

--- next.config.js
+++ next.config.js
@@ -6,15 +6,9 @@ const isProd = (process.env.NODE_ENV || 'production') === 'production';
 const assetPrefix = isProd ? '/sturdy-umbrella' : '';
 
 module.exports = {
-  assetPrefix: assetPrefix,
   pageExtensions: ['jsx', 'md', 'mdx'],
   distDir: 'build',
   webpack: (config, { defaultLoaders }) => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
-      })
-    );
     config.module.rules.push({
       test: /\.jsx?$/,
       exclude: /node_modules/,
@@ -34,6 +28,18 @@ module.exports = {
 
     config.resolve.alias.components = path.join(__dirname, './components');
 
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+      })
+    );
+
     return config;
   },
+  exportPathMap: () => ({
+    '/': { page: '/' },
+    '/Checkbox': { page: '/Checkbox' },
+    '/TextField': { page: '/TextField' },
+  }),
+  assetPrefix: assetPrefix,
 };
