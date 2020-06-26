cec344611e1ff5a1843986c96019d576a5369609
menkuichan <ilonaspolding@gmail.com>
2020-06-26 16:28:46 +0300

    feat: Add daytime check

---
+++ docs/utils/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/prefer-default-export */
+import SunCalc from 'suncalc';
+import isWithinInterval from 'date-fns/isWithinInterval';
+import { theme } from '../../constants';
+
+export const checkTheme = () => {
+  const currentDate = new Date();
+  let currentLatitude = '';
+  let currentLongitude = '';
+
+  navigator.geolocation.getCurrentPosition(
+    ({ coords: { latitude, longitude } }) => {
+      currentLatitude = latitude;
+      currentLongitude = longitude;
+    }
+  );
+
+  const { sunrise, sunset } = SunCalc.getTimes(
+    currentDate,
+    currentLatitude,
+    currentLongitude
+  );
+
+  const isDayTime = isWithinInterval(currentDate, {
+    start: sunrise,
+    end: sunset,
+  });
+  if (isDayTime) {
+    return theme.light;
+  }
+  return theme.dark;
+};
